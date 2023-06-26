c29aff789104a13cc1e54440a92adee2e907a88f
tskawada <yuta_kawada@techsword.co.jp>
2023-06-26 13:30:48 +0900

    large view modal

--- frontend/src/components/ImagePreview.tsx
+++ frontend/src/components/ImagePreview.tsx
@@ -1,5 +1,6 @@
-import { Box, Stack, Typography } from '@mui/material'
+import { Modal, Box, Stack, Typography } from '@mui/material';
 import { AspectRatio } from '@mui/joy';
+import React, { useState } from 'react';
 
 type ImagePreviewProps = {
   rawImage: string | undefined;
@@ -10,46 +11,68 @@ type ImagePreviewProps = {
     rawImage,
     starlessImage,
   }: ImagePreviewProps) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
+  interface ImageBoxProps {
+    src: string | undefined;
+  }
 
-    interface ImageBoxProps {
-      src: string | undefined;
-    }
-
-    const ImageBox = (props: ImageBoxProps) => {
-      return (
-        <Box 
-          component="img"
-          sx={{
-            width: 1/2, 
-            borderRadius: 1
-          }}
-          src={ props.src ? props.src : undefined }
-        />
-      );
-    };
-
-    const NoContent = () => {
-      return (
-        <AspectRatio ratio="4/3" sx={{ width: 1/2, borderRadius: 1 }}>
-          <Typography variant="h2" component="div">
-            No Content
-          </Typography>
-        </AspectRatio>
-      );
-    };
-
+  const ImageBox = (props: ImageBoxProps) => {
     return (
-        <Stack 
-            direction="row" 
-            spacing={0.5}
-            sx={{ m: 1 }}
-            alignItems="center" 
-            justifyContent="space-evenly"
-        >
-            { rawImage ? <ImageBox src={rawImage} /> : <NoContent /> }
-            { starlessImage ? <ImageBox src={starlessImage} /> : <NoContent /> }
-        </Stack>
+      <Box 
+        sx={{
+          width: 1/2, 
+          borderRadius: 1
+        }}
+        onClick={handleOpen}
+      >
+        <img src={ props.src ? props.src : undefined } style={{ width: '100%' }} />
+        <Modal open={open} onClose={handleClose}>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              justifyContent: 'center',
+              height: '90%',
+              bgcolor: 'background.paper',
+              boxShadow: 24,
+              p: 2,
+              borderRadius: 1,
+            }}
+          >
+            <img src={ props.src ? props.src : undefined } style={{ height: '100%' }} />
+          </Box>
+        </Modal>
+      </Box>
     );
+  };
+
+  const NoContent = () => {
+    return (
+      <AspectRatio ratio="8/5" sx={{ width: 1/2, borderRadius: 1 }}>
+        <Typography variant="h2" component="div">
+          No Content
+        </Typography>
+      </AspectRatio>
+    );
+  };
+
+  return (
+      <Stack 
+          direction="row" 
+          spacing={0.5}
+          sx={{ m: 1 }}
+          alignItems="center" 
+          justifyContent="space-evenly"
+      >
+          { rawImage ? <ImageBox src={rawImage} /> : <NoContent /> }
+          { starlessImage ? <ImageBox src={starlessImage} /> : <NoContent /> }
+      </Stack>
+  );
 };
 
 export default ImagePreview
